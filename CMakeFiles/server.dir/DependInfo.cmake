86fd80ac32a523d7ad92475cb268846dc7c4efb4
magistermaks <magistermaks@darktree.net>
2024-04-22 12:44:48 +0200

    Move to CMake

---
+++ CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/magistermaks/Documents/Dev/urp-git/src/common/input.c" "CMakeFiles/server.dir/src/common/input.c.o" "gcc" "CMakeFiles/server.dir/src/common/input.c.o.d"
+  "/home/magistermaks/Documents/Dev/urp-git/src/common/logger.c" "CMakeFiles/server.dir/src/common/logger.c.o" "gcc" "CMakeFiles/server.dir/src/common/logger.c.o.d"
+  "/home/magistermaks/Documents/Dev/urp-git/src/common/map.c" "CMakeFiles/server.dir/src/common/map.c.o" "gcc" "CMakeFiles/server.dir/src/common/map.c.o.d"
+  "/home/magistermaks/Documents/Dev/urp-git/src/common/set.c" "CMakeFiles/server.dir/src/common/set.c.o" "gcc" "CMakeFiles/server.dir/src/common/set.c.o.d"
+  "/home/magistermaks/Documents/Dev/urp-git/src/common/stream.c" "CMakeFiles/server.dir/src/common/stream.c.o" "gcc" "CMakeFiles/server.dir/src/common/stream.c.o.d"
+  "/home/magistermaks/Documents/Dev/urp-git/src/common/util.c" "CMakeFiles/server.dir/src/common/util.c.o" "gcc" "CMakeFiles/server.dir/src/common/util.c.o.d"
+  "/home/magistermaks/Documents/Dev/urp-git/src/common/vec.c" "CMakeFiles/server.dir/src/common/vec.c.o" "gcc" "CMakeFiles/server.dir/src/common/vec.c.o.d"
+  "/home/magistermaks/Documents/Dev/urp-git/src/server/config.c" "CMakeFiles/server.dir/src/server/config.c.o" "gcc" "CMakeFiles/server.dir/src/server/config.c.o.d"
+  "/home/magistermaks/Documents/Dev/urp-git/src/server/group.c" "CMakeFiles/server.dir/src/server/group.c.o" "gcc" "CMakeFiles/server.dir/src/server/group.c.o.d"
+  "/home/magistermaks/Documents/Dev/urp-git/src/server/mutex.c" "CMakeFiles/server.dir/src/server/mutex.c.o" "gcc" "CMakeFiles/server.dir/src/server/mutex.c.o.d"
+  "/home/magistermaks/Documents/Dev/urp-git/src/server/sequence.c" "CMakeFiles/server.dir/src/server/sequence.c.o" "gcc" "CMakeFiles/server.dir/src/server/sequence.c.o.d"
+  "/home/magistermaks/Documents/Dev/urp-git/src/server/server.c" "CMakeFiles/server.dir/src/server/server.c.o" "gcc" "CMakeFiles/server.dir/src/server/server.c.o.d"
+  "/home/magistermaks/Documents/Dev/urp-git/src/server/store.c" "CMakeFiles/server.dir/src/server/store.c.o" "gcc" "CMakeFiles/server.dir/src/server/store.c.o.d"
+  "/home/magistermaks/Documents/Dev/urp-git/src/server/tcps.c" "CMakeFiles/server.dir/src/server/tcps.c.o" "gcc" "CMakeFiles/server.dir/src/server/tcps.c.o.d"
+  "/home/magistermaks/Documents/Dev/urp-git/src/server/user.c" "CMakeFiles/server.dir/src/server/user.c.o" "gcc" "CMakeFiles/server.dir/src/server/user.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
